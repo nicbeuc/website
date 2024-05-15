f487f2357f49b7051977c2de412bae9d5b594bf5
Nick Beuchat <nick.beuchat@gmail.com>
2024-05-15 16:47:37 -0400

    small stuff

---
+++ src/routes/now/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import InlineLink from "$lib/InlineLink.svelte";
+</script>
+
+<article>
+  <h1 class="page-title">What I'm Doing Now</h1>
+  <section>
+    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
+  </section>
+</article>
+
+<style>
+
+</style>
