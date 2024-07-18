c0ee695619899fcc9626c3dd43d1e976a62c35c9
Nick Beuchat <nick.beuchat@gmail.com>
2024-07-17 20:25:47 -0400

    better error handling at project page level

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,1 @@
-<h1>Test</h1>
+<h1>Page not found.</h1>
--- src/routes/project/[slug]/+page.js
+++ src/routes/project/[slug]/+page.js
@@ -1,24 +1,28 @@
 export const load = async ({ params }) => {
-	const project = await import(`../../../projects/${params.slug}.md`);
-	const {
-    client,
-    thumbnail,
-    company,
-    lead,
-    link,
-    year,
-    tags
-  } = project.metadata;
-	const content = project.default;
+  try {
+    const project = await import(`../../../projects/${params.slug}.md`);
+    const {
+      client,
+      thumbnail,
+      company,
+      lead,
+      link,
+      year,
+      tags
+    } = project.metadata;
+    const content = project.default;
 
-	return {
-    client,
-    thumbnail,
-    company,
-    lead,
-    link,
-    year,
-    tags,
-    content
-	};
+    return {
+      client,
+      thumbnail,
+      company,
+      lead,
+      link,
+      year,
+      tags,
+      content
+    };
+  } catch (error) {
+    throw new Error('Project not found.');
+  }
 }
