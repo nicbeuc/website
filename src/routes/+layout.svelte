b0f69d8d0962290cf27553ce1d62eb2cdead367d
Nick Beuchat <nick.beuchat@gmail.com>
2024-07-20 12:41:04 -0400

    images with captions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -76,11 +76,11 @@
 
   @media (prefers-reduced-motion: no-preference) {
     :root::view-transition-old(root) {
-      animation: 100ms cubic-bezier(0.4, 0, 1, 1) both fade-out, 350ms cubic-bezier(0.4, 0, 0.2, 1) both slide-up;
+      animation: 100ms cubic-bezier(0.4, 0, 1, 1) both fade-out, 350ms cubic-bezier(0.370, 0.265, 0.025, 1.000) both slide-up;
     }
 
     :root::view-transition-new(root) {
-      animation: 250ms cubic-bezier(0, 0, 0.2, 1) 100ms both fade-in, 350ms cubic-bezier(0.4, 0, 0.2, 1) both
+      animation: 250ms cubic-bezier(0, 0, 0.2, 1) 100ms both fade-in, 350ms cubic-bezier(0.370, 0.265, 0.025, 1.000) both
           slide-down;
     }
   }
--- src/routes/project/[slug]/+page.svelte
+++ src/routes/project/[slug]/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+  import Link from '$lib/Link.svelte';
+  import Image from '$lib/Image.svelte';
+  import Icon from '$lib/Icon.svelte';
   import { metaContent as meta } from '$/constants';
 	export let data;
 
@@ -29,7 +32,14 @@
       <dt>Year</dt>
       <dd>{year}</dd>
     </dl>
+    {#if link}
+      <Link href={link} external>View live site</Link>
+    {/if}
   </header>
+  <figure>
+    <Image alt="Image of the {client} site" name={thumbnail} />
+    <figcaption><Icon name="image" />A look at the completed site</figcaption>
+  </figure>
 	<svelte:component this={content} />
 </article>
 
@@ -38,15 +48,18 @@
     padding-bottom: 3.2rem;
     margin-bottom: 3.2rem;
     border-bottom: var(--border);
+    display: grid;
+    grid-template-columns: 1fr auto;
+    grid-template-areas:
+      "title title"
+      "meta   link";
     & h1 {
       grid-area: title;
-      margin-inline-end: 1.6rem;
     }
 
-    & p {
-      grid-area: year;
-      color: var(--color-neutral-600);
-      font-size: var(--font-size-body-sm);
+    & a {
+      grid-area: link;
+      align-self: end;
     }
   }
 
@@ -54,6 +67,7 @@
     font-size: var(--font-size-body-sm);
     color: var(--color-neutral-600);
     display: grid;
+    grid-area: meta;
     grid-auto-flow: column dense;
     grid-template-columns: auto;
     grid-template-rows: auto auto;
