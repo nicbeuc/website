ab35a560a4d30cf208d04b45cd429634440cd2ae
Nick Beuchat <nick.beuchat@gmail.com>
2024-09-07 11:05:31 -0400

    copy email to clipboard

---
+++ src/lib/CopyEmail.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Icon from "$lib/Icon.svelte";
+
+  function handleClick() {
+    navigator.clipboard.writeText('nick.beuchat@gmail.com');
+    alert("Email copied to clipboard.");
+  }
+</script>
+
+<button on:click={handleClick}>
+  <span class="sr-only">Copy email to clipboard</span>
+  <span aria-hidden="true"><slot /></span>
+  <Icon name="copy" --icon-size=".8em"/>
+</button>
+
+<style>
+  button {
+    color: var(--color-neutral-600);
+    transition: color var(--transition-fast), font-weight var(--transition-fast);
+    display: inline-flex;
+    align-items: center;
+    position: relative;
+    gap: .6rem;
+    border: 0;
+    padding: .4rem 0;
+    background-color: transparent;
+    cursor: pointer;
+
+    &:hover {
+      color: inherit;
+
+      & * {
+        color: inherit;
+      }
+    }
+
+    & svg {
+      color: var(--color-neutral-500);
+      display: inline-block;
+      transition: color var(--transition-fast);
+    }
+  }
+</style>
