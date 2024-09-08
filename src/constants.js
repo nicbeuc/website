b92e0160ba08479f8ba75cb0024debd4ef35edb2
Nick Beuchat <nick.beuchat@gmail.com>
2024-09-08 17:59:03 -0400

    some data

--- src/constants.js
+++ src/constants.js
@@ -11,3 +11,43 @@ export const metaContent = {
 	title: 'Nick Beuchat · Designer & Creative Developer',
 	ogImage: 'https://nickbeuchat.com/og-image.jpg'
 };
+
+export const otherProjects = [
+	{
+		title: 'Shack Store - Shake Shack Merch',
+		client: 'Shake Shack',
+		info: `A Shopify store for Shake Shack's line of merchandise.`,
+		link: 'https://shackstore.shakeshack.com/',
+		year: '2023',
+		team: 'Make Me Modern',
+		roles: ['Design', 'Development']
+	},
+	{
+		title: 'Shop The Sims',
+		client: 'Electronic Arts',
+		info: 'A Shopify store for a line of merchandise that celebrates The Sims, one of the best selling video games of all time.',
+		link: 'https://shopthesims.com/',
+		year: '2023',
+		team: 'Make Me Modern',
+		roles: ['Design', 'Development']
+	},
+	{
+		title: 'Breezy Golf',
+		client: 'Doing Things Media',
+		info: '',
+		link: 'https://breezygolf.com/',
+		year: '2023',
+		team: 'Make Me Modern',
+		roles: ['Design, Development']
+	},
+	{
+		title: 'Dr. Noze Best',
+		client: 'Dr. Noze Best',
+		info: '',
+		link: 'https://drnozebest.com/',
+		year: '2023',
+		team: 'Make Me Modern',
+		roles: ['Design', 'Development', 'Project Management']
+	}
+	// Wet Tennis
+];
---
+++ src/lib/ProjectLink.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<article>
+
+</article>
+
+<style>
+
+</style>
