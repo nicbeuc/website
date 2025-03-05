78ea8b8142024bdd8be5b38d6c67a9adae4cc9c5
Nick Beuchat <nick.beuchat@gmail.com>
2025-03-04 21:15:18 -0500

    new logo lol

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -1,18 +1,20 @@
-<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label="Minimal 'N' logo">
-  <path d="M16.8422 0H19.8422C26.4696 0 31.8422 5.37258 31.8422 12V32H22.8422C19.5285 32 16.8422 29.3137 16.8422 26V0Z" fill="currentColor"/>
-  <path d="M0 0H15V26C15 29.3137 12.3137 32 9 32H0V0Z" fill="currentColor"/>
+<svg width="32" height="26" viewBox="0 0 32 26">
+	<path
+		fill="currentColor"
+		d="M24.276 0c-.343 0-.68.082-.987.239a2.22 2.22 0 0 0-.779.665l-5.958 8.14h4.414a2.18 2.18 0 0 1 1.56.662c.414.424.646.999.646 1.598V26H32V0h-7.724ZM8.828 6.783V0H0v9.043h2.207a2.18 2.18 0 0 1 1.56.663c.414.424.647.999.647 1.598v6.783h4.414c.342 0 .68-.082.987-.239a2.22 2.22 0 0 0 .778-.665l5.959-8.14h-5.518a2.18 2.18 0 0 1-1.56-.662 2.29 2.29 0 0 1-.646-1.598Z"
+	/>
 </svg>
 
 <style>
-  svg {
-    height: 3.2rem;
-    width: 3.2rem;
-    color: black;
-    justify-self: center;
+	svg {
+		height: auto;
+		aspect-ratio: 32/26;
+		width: 3rem;
+		color: black;
+		justify-self: center;
 
-    @media screen and (max-width: 799px) {
-      height: 2.4rem;
-      width: 2.4rem;
-    }
-  }
+		@media screen and (max-width: 799px) {
+			width: 2.4rem;
+		}
+	}
 </style>
